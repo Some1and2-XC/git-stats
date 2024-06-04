78c1b024c6e95b06511f269d5d8e101a763978f9
MT <04x0xx@gmail.com>
2024-06-03 23:20:45 -0300

    patch: Fixed file searching

--- src/packfiles.rs
+++ src/packfiles.rs
@@ -52,12 +52,12 @@ impl BytesFile {
     }
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy)]
 pub struct Hash(pub [u8;HASH_SIZE]);
 
 impl Hash {
     pub fn to_string(&self) -> String {
-        let values: [char;16] = [
+        const HEX_VALUES: [char;16] = [
             '0','1','2','3',
             '4','5','6','7',
             '8','9','a','b',
@@ -70,13 +70,13 @@ impl Hash {
                 // this means however that each value being iterated on is actually two characters
                 // in utf-8
                 let left = idx >> 4; // Does bit shifting to filter
-                let right = idx & b'\x0f'; // Filters value
+                let right = idx & b'\x0f'; // Filters value for first 4 values
 
                 // Returns left & right
                 return format!(
                     "{}{}",
-                    values[left as usize],
-                    values[right as usize],
+                    HEX_VALUES[left as usize],
+                    HEX_VALUES[right as usize],
                     );
             }).collect();
     }
@@ -93,7 +93,6 @@ pub struct Idx {
     pub version: u32,
     pub file: BytesFile,
 }
-
 
 impl Idx {
     // Creates a new Idx file from path.
@@ -179,7 +178,7 @@ impl Idx {
         // Skips magic byte, version and sets offset.
         self.file.data.seek(SeekFrom::Start(
             4 + 4 +
-            offset * 4
+            offset
             ))?;
         return Ok(());
     }
@@ -205,7 +204,7 @@ impl Idx {
             self.seek_without_headers(0)?;
             0
         } else {
-            self.seek_without_headers(first_hash_byte as u64 - 1)?;
+            self.seek_without_headers(first_hash_byte as u64 * 4 - 4)?;
             self.file.read_u32()?
         };
         let index_upper_bound = self.file.read_u32()?;
@@ -216,11 +215,13 @@ impl Idx {
         use std::cmp::Ordering::*;
         let (mut left, mut right) = self.get_object_bounds(&hash)?;
         debug!("B-Tree Searching for Hash: {hash}! left: {left} right: {right}");
+
         // Does binary search
         while left < right {
             let middle = left + (right - left) / 2;
             self.seek_without_index(middle as u64)?;
             let mid_hash = self.file.read_hash()?;
+
             match hash.cmp(&mid_hash) {
                 Less => {
                     debug!("{} {mid_hash} {left} {right}", "<-".red());
@@ -239,16 +240,21 @@ impl Idx {
 
     fn get_pack_offset_at_index(&mut self, offset: u32) -> Result<u64> {
         // Gets the total amount of objects
-        self.seek_without_headers(u8::MAX as u64)?;
+        self.seek_without_headers(u8::MAX as u64 * 4)?;
         let total_object_count = self.file.read_u32()?;
-        let crc_32_bytes = total_object_count * 4;
-        self.seek_without_headers(
-            (crc_32_bytes +
-            offset * 4) as u64,
-            )?;
-        let pack_offset = self.file.read_u32()?;
+        debug!("Finding file '{offset}' of '{total_object_count}'");
+
+        let crc_32_bytes = total_object_count * (HASH_SIZE as u32 + 4);
+
+        let file_index = (crc_32_bytes + offset * 4) as u64;
+        debug!("File Index: '{file_index}'");
+
+        self.seek_without_headers(file_index).unwrap();
+        let pack_offset = self.file.read_u32().unwrap();
+        debug!("Pack offset: '{pack_offset}'");
         // If uses long offsets, read long offsets.
         if pack_offset & LONG_OFFSET_FLAG == 0 {
+            debug!("Unpacking offset value without long offset flag.");
             return Ok(pack_offset as u64);
         } else {
             let offset_index = pack_offset & !LONG_OFFSET_FLAG;
@@ -256,6 +262,7 @@ impl Idx {
                 (crc_32_bytes +
                 offset * 8) as u64,
                 )?;
+            debug!("Unpacking offset value with long offset flag.");
             return self.file.read_u64();
         }
     }
@@ -306,7 +313,7 @@ impl Pack {
             None => return Ok(None),
         };
 
-        let pack_offset = index.get_pack_offset_at_index(object_index)?;
+        let pack_offset = index.get_pack_offset_at_index(object_index).unwrap();
         return Ok(Some(pack_offset));
     }
 
@@ -323,7 +330,7 @@ impl Pack {
         // Gets the first bit which is the continue flag
         const VARIANT_ENCODING_CONTINUE_FLAG: u8 = 1 << VARIANT_ENCODING_BITS;
 
-        let [byte] = self.file.read_bytes()?;
+        let [byte] = self.file.read_bytes().unwrap();
         let value = byte & !VARIANT_ENCODING_CONTINUE_FLAG; // Gets the value without continue bit
         let more_bytes = byte & VARIANT_ENCODING_CONTINUE_FLAG != 0; // Gets continue flag
 
@@ -336,7 +343,7 @@ impl Pack {
         const VARIANT_ENCODING_BITS: u8 = Pack::get_encoding_bits();
 
         loop {
-            let (bytes_value, more_bytes) = self.read_variant_byte()?;
+            let (bytes_value, more_bytes) = self.read_variant_byte().unwrap();
 
             // Adds bytes to the output 'value' var
             // Note this does reverse the order of bytes read
@@ -351,18 +358,25 @@ impl Pack {
     }
 
     pub fn read_pack_object(&mut self, offset: u64) -> Result<()> {
-        self.file.data.seek(SeekFrom::Start(offset)).unwrap();
+        self.file.data.seek(SeekFrom::Start(offset))?;
         let type_and_size = self.read_size_encoding().unwrap();
         debug!("Type and Size: {type_and_size:b}");
         return Ok(());
     }
 
     pub fn run(&mut self) -> Result<()> {
-        let hash = Hash(
-            b"\x53\x4e\x71\xdc\x55\xa7\xf4\x90\xdc\xd3\xaa\x53\x4f\x16\x27\x1c\xda\x92\xab\xeb".to_owned());
-        let offset = self.get_pack_offset(hash).unwrap().unwrap();
-        debug!("Offset: {offset:?}");
-        self.read_pack_object(offset).unwrap();
+        let hashes: Vec<Hash> = vec![
+            // Hash(b"\x53\x71\x41\x95\xca\xec\x3f\xdb\xca\xf2\x1d\x4f\x1e\xc5\x19\x11\xfd\x2d\x5c\xb6".to_owned()), // 30677134653415695
+            // Hash(b"\x53\x43\x95\x3d\xa4\xcf\x06\x59\xa2\x5f\x63\xd0\x33\x87\xe7\x78\x6e\x89\x0e\xd1".to_owned()), // 122881307880357819
+            // Hash(b"\x53\x4e\x71\xdc\x55\xa7\xf4\x90\xdc\xd3\xaa\x53\x4f\x16\x27\x1c\xda\x92\xab\xeb".to_owned()), // 926845331
+            Hash(b"\x53\x53\xbc\xbe\xa8\xa1\x73\x77\x2a\x21\x17\x72\x9f\xe0\x6c\x6b\xf7\x35\xd0\x1f".to_owned()),
+        ];
+
+        for &hash in hashes.iter() {
+            let offset = self.get_pack_offset(hash).unwrap().unwrap();
+            debug!("Offset: {offset:?}");
+            self.read_pack_object(offset).unwrap();
+        }
         return Ok(());
     }
 }
