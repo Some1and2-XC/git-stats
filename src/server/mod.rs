fa89784d7317bc8e58a05a41c122e786cf78ab36
MT <04x0xx@gmail.com>
2024-06-25 17:14:21 -0300

    patch: Changed to http parsing lib for parsing paths

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,21 +1,32 @@
-use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, path::{Path, PathBuf}};
+use std::{fs, io::{BufRead, BufReader, Read, Write}, net::{TcpListener, TcpStream}, path::{Path, PathBuf}};
 use regex::Regex;
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use httparse;
 
 use crate::{cli::cli, OutputValue};
 
-fn get_path(in_str: &Vec<String>) -> Result<String> {
-    let re = Regex::new("GET (?<path>.+?) HTTP/1.1").unwrap();
-    return Ok(
-        re.captures(
-            in_str
-                .first()
-                .with_context(|| "Expected first value")?
-            )
-            .with_context(|| "Expected regex match")?
-            .name("path")
-            .unwrap()
-            .as_str().to_string());
+fn get_path(reader: &mut BufReader<&mut TcpStream>) -> Result<String> {
+    // fills buffer
+    let mut buf: Vec<u8> = Vec::new();
+    let _buf_len = reader.read_until(b'\r', &mut buf).unwrap();
+
+    // Sets header stuff
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+
+    // Parses headers
+    let _ = req.parse(&buf).unwrap();
+
+    // Tries to resolve header
+    match req.path {
+        Some(path) => {
+            log::info!("Parsed path: '{path}' from request.");
+            return Ok(path.to_string());
+        },
+        None => {
+            return Err(anyhow!("Path not found in request body! Request: '{:?}'.", buf));
+        }
+    }
 }
 
 enum OutputType {
@@ -24,14 +35,9 @@ enum OutputType {
 }
 
 pub fn handle_connection(mut stream: TcpStream, path: &str, args: &cli::CliArgs) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|res| res.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let mut buf_reader = BufReader::new(&mut stream);
 
-    let out_path = get_path(&http_request).unwrap_or("/404".to_string());
+    let out_path = get_path(&mut buf_reader).unwrap_or("/404".to_string());
     let output_value: OutputType;
 
     if out_path == "/api/data" {
@@ -39,7 +45,7 @@ pub fn handle_connection(mut stream: TcpStream, path: &str, args: &cli::CliArgs)
     } else if out_path == "/" {
         output_value = OutputType::File("/index.html".to_string());
     } else {
-        output_value = OutputType::File(out_path);
+        output_value = OutputType::File(out_path.clone());
     }
 
     let (status_line, contents) = match output_value {
@@ -74,6 +80,4 @@ pub fn handle_connection(mut stream: TcpStream, path: &str, args: &cli::CliArgs)
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-
-    println!("Request: {:#?}", http_request);
 }
